fda7d43948a6a5f4ea7708eed4b5da81f170cfdd
Seungwon Park <psw14041@gmail.com>
2016-09-17 12:21:53 +0900

    download - <br>, navbar

--- download.php
+++ download.php
@@ -15,17 +15,17 @@ description: "LaTeX is a high-quality typesetting system; it includes features d
 <div class="row">
 	<div class="col cell1of3">
 		<a href="files/gshslatexintro.zip" class="btn"><b>양식</b>(<?php echo $size_1 ?>MB)</a>
-		
+		<br>
 		R&E, 졸업논문, 휴텍 양식 등
 	</div>
 	<div class="col cell1of3">
 		<a href="files/examples.zip" class="btn"><b>예시 코드</b>(<?php echo $size_2 ?>MB)</a>
-		
+		<br>
 		매나니 화성학 강의 등
 	</div>
 	<div class="col cell1of3">
 		<a href="files/An-Introduction-to-LaTeX.zip" class="btn"><b>텍 입문서</b>(<?php echo $size_3 ?>MB)</a>
-		
+		<br>
 		v2.0 - Day0, Day1, Day2
 	</div>
 </div>
